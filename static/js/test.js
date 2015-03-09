4a9cb2f1efc78a5f0d02330101046b51d649cb88
Nofcale <juanpestana96@gmail.com>
2015-03-09 17:36:54 -0500

    Ver Presupuesto aparece cuando se edita no cuando se agrega

--- static/js/test.js
+++ static/js/test.js
@@ -7,7 +7,6 @@ window.onload = function() {
 	var submitRow = document.getElementsByClassName('submit-row');
 	var deleteButton = document.getElementsByClassName('deletelink');
 
-
 	var urlActual = document.URL;
 	var urlSplitted = urlActual.split('/');
 
@@ -15,7 +14,12 @@ window.onload = function() {
 
 	div.innerHTML= '<button type="button" onclick="location.href=\'/admin/presupuesto/'+proyectoId+'\'" class="btn btn-high btn-success" style="margin-top:20px;" name="pre"><i class="icon-list icon-white"></i>&nbsp;&nbsp;Ver Presupuesto</button>';
 
-	submitRow[0].appendChild(div);
+	if (proyectoId == "add") {
+		// Es cuando se agrega así que no se pone el botón de Ver Presupuesto
+	}else{
+		submitRow[0].appendChild(div);		
+	}
+	
 
 }
 

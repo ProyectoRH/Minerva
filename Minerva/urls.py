cfc21913ed57d4b3703d45006acb73029fcc7843
Juan Pestana <juanpestana96@gmail.com>
2015-05-21 14:16:58 -0500

    Añadido name en urls

--- Minerva/urls.py
+++ Minerva/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = patterns('',
     url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', {'post_reset_redirect' : '/password_reset/done/'}, name="password_reset"),
     url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
-    (r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
-    (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', name="password_reset_confirm"),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name="password_reset_complete"),
 )
 
 

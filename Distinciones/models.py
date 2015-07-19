d8fedd94e3e6e81272c5482049c15ff2e7d6729d
Juan Pestana <juanpestana96@gmail.com>
2015-07-19 07:44:20 -0500

    UTF-8 para distinciones

--- Distinciones/models.py
+++ Distinciones/models.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from Entidad.models import Entidad, AlcanceMercado
 # Create your models here.
 
 class Distincion(models.Model):
 	entidad = models.ForeignKey(Entidad)
-	reconocimiento = models.CharField(max_length=255, null=True, blank=True)
+	reconocimiento = models.TextField(null=True, blank=True)
 	fecha = models.DateField(null=True, blank=True)
 	alcance = models.ForeignKey(AlcanceMercado, null=True, blank=True)
 	creacion = models.DateField(auto_now_add=True)
